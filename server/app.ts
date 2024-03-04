312347fca2b7172a1ecab2545bbb6e7caae5a16a
Eli Marshall <eli.marshall@unosquare.com>
2024-03-04 13:04:15 -0500

    Add swagger

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,42 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 import express from 'express';
 import cors from 'cors';
 import { authRouter, eventsRouter, ticketsRouter } from './routers';
 import { verifyToken } from './middleware/auth';
+import swaggerJsdoc from 'swagger-jsdoc';
+const swaggerUI = require('swagger-ui-express');
+
+const options = {
+  failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Event Management API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routers/**.ts', `${__dirname}/routers/*.ts`],
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+console.log(openapiSpecification);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+/**
+ * Public Routes
+ */
 app.use('/auth', authRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openapiSpecification));
 
+/**
+ * Protected Routes
+ */
 app.use(verifyToken);
 app.use('/events', eventsRouter);
 app.use('/tickets', ticketsRouter);
--- server/routers/auth.ts
+++ server/routers/auth.ts
@@ -1,9 +1,75 @@
 import { Router } from 'express';
 import * as authController from '../controllers/auth';
 
+/**
+ * @swagger
+ * definitions:
+ *   Login:
+ *     required:
+ *       - email
+ *       - password
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ *       path:
+ *         type: string
+ */
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: User management and Auth
+ */
 const router = Router();
 
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     summary: Sign in a user.
+ *     tags: [Auth]
+ *     parameters:
+ *       - name: email
+ *         description: User's email.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious json message.
+ */
 router.route('/signin').post(authController.postSignIn);
+
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     summary: Register a new user 
+ *     tags: [Auth]
+ *     parameters:
+ *       - name: email
+ *         description: User's email.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious json message.
+ */
 router.route('/signup').post(authController.postSignUp);
 
 export default router;
