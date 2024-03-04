d540f11599831d21f4920af7b4c72f9d41bbfd37
Eli Marshall <eli.marshall@unosquare.com>
2024-03-04 11:19:35 -0500

    Add Middleware

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { authRouter, eventsRouter } from './routers';
+import { authRouter, eventsRouter, ticketsRouter } from './routers';
+import { verifyToken } from './middleware/auth';
 
 const app = express();
 
 app.use('/auth', authRouter);
+
+app.use(verifyToken);
 app.use('/events', eventsRouter);
-app.use('/tickets', eventsRouter);
+app.use('/tickets', ticketsRouter);
 
 export { app };
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,9 @@
+import { app } from './app';
 import express from 'express';
+import cors from 'cors';
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
