606d3ecc8dcbcaa4c9b43b4bce128d6e1fd224c1
Eli Marshall <eli.marshall@unosquare.com>
2024-03-04 12:06:23 -0500

    Move cors implementation

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { authRouter, eventsRouter, ticketsRouter } from './routers';
 import { verifyToken } from './middleware/auth';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.use('/auth', authRouter);
 
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,4 @@
 import { app } from './app';
-import express from 'express';
-import cors from 'cors';
-
-app.use(cors());
-app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
