976e13b19ddfcf149c0f67a5a913bcd58ed2904e
Eli Marshall <eli.marshall@unosquare.com>
2024-03-19 14:34:28 -0400

    Trigger workflow run

--- database/migrations/V1.0__initial_schema.sql
+++ database/migrations/V1.0__initial_schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS public.users (
     identifier VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
     title VARCHAR(255),
+    address VARCHAR(255),
     password VARCHAR(255) NOT NULL
 );
 
